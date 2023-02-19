a5dcd48225f43fcc08f55755c9ff30f511c2d702
marcsouth <104800775+marcsouth@users.noreply.github.com>
2023-02-19 00:33:37 -0500

    image saving change

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ jQuery(document).ready(function ($) {
         responsive: [{
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 1,
                 slidesToScroll: 1
             }
         },
